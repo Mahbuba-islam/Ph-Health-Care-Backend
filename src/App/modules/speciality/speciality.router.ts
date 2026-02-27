5f8cde1478d139555a83c27bb392c0e43bc286c3
Mahbuba Islam <mahbubaislam7010@gmail.com>
2026-02-27 16:51:28 -0500

    use-cloudinary

--- src/App/modules/speciality/speciality.router.ts
+++ src/App/modules/speciality/speciality.router.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { specialityControler } from "./speciality.controler";
+import { checkAuth } from "../../middleware/cheackAuth";
+import { Role } from "../../../generated/prisma/enums";
+import { multerUpload } from "../../../config/multer.config";
 
 const router = Router()
 
-router.post("/", specialityControler.createSpeciality)
+router.post("/", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), multerUpload.single("file"), specialityControler.createSpeciality)
 router.get("/", specialityControler.getAllSpeciality)
 router.delete("/:id", specialityControler.deleteSpeciality)
 router.put("/:id", specialityControler.updateSpeciality)

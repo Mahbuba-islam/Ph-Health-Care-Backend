dc7544e8145a29e4cd89ae67728ea1284ee99c22
Mahbuba Islam <mahbubaislam7010@gmail.com>
2026-02-28 06:32:39 -0500

    upload-file

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -1,7 +1,8 @@
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 import { envVars } from "./env";
 import AppError from "../App/errorHelpers/AppError";
 import status from "http-status";
+import { resolve } from "node:dns";
 
 cloudinary.config({
   cloud_name: envVars.CLOUDINARY.CLOUDINARY_CLOUD_NAME,
@@ -32,5 +33,57 @@ export const deleteFileFromCloudinary = async(url:string) => {
   }
  
 }
+
+
+
+//manual function for file upload
+export const uploadFileTocloudinary = async(
+  buffer:Buffer,
+  fileName:string
+) : Promise<UploadApiResponse> => {
+
+
+  if(!buffer || !fileName){
+    throw new AppError(status.BAD_REQUEST, "File Buffer and File Name are required")
+  }
+
+
+   
+    const extention = fileName.split(".").pop()?.toLocaleLowerCase();
+
+    const fileNameWithoutExtension = fileName
+    .split(".")
+    .slice(0, -1)
+    .join(".")
+    .toLocaleLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^a-z0-9/-]/g, "")
+
+    const uniqueName = Math.random().toString(36).substring(2)+ "-" +Date.now()+ "-"+ fileNameWithoutExtension
+
+    const folder = extention === "pdf"? "pdfs" : "images";
+
+    return new Promise((resolve, reject) => {
+     cloudinary.uploader.upload_stream({
+      folder: `ph-health-care/${folder}`,
+     public_id:uniqueName,
+     resource_type: "auto"
+     },
+
+     (error, result)=>{
+      if(error){
+        return reject(new AppError(status.INTERNAL_SERVER_ERROR, "failed to upload file"))
+      }
+
+      resolve(result as UploadApiResponse)
+     }
+    ).end(buffer)
+
+    }) 
+  
+}
+
+
+
 
 export const cloudinaryUpload = cloudinary;
